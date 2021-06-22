fb4c9997a3024e3185648ec3f49a343ae519c2e9
kaikaigame <kairemake@outlook.com>
2021-06-23 03:14:33 +0900

    Initial commit

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Completed;
+
+public class Loader : MonoBehaviour
+{
+    //GameManager prefab to instantiate.
+    public GameObject gameManager;
+
+    //SoundManager prefab to instantiate.
+    public GameObject soundManager;
+
+    void Awake()
+    {
+        //Check if a GameManager has already been assigned 
+        //to static variable GameManager.
+        //instance or if it's still null
+        if (GameManager.instance == null)
+            //Instantiate gameManager prefab
+            Instantiate(gameManager);
+
+        //Check if a SoundManager has already been assigned 
+        //to static variable SoundManager.
+        //instance or if it's still null
+        if (SoundManager.instance == null)
+            //Instantiate SoundManager prefab
+            Instantiate(soundManager);
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# 2D Roguelike
+  
+Just the practice.  
+Not available for commercial use.  
+  
+Free Assets downloaded from  
+  
+https://learn.unity.com/project/2d-roguelike-tutorial  
+  
+https://assetstore.unity.com/packages/essentials/tutorial-projects/2d-roguelike-29825  
+    
+Unity m_EditorVersion:  2021.1.12f1  
+  
+KEYBOARD  
+  
+UP: w or up  
+DOWN: s or down  
+LEFT: a or left  
+RIGHT: d or right  
+  
